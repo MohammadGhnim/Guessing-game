f949d3eb93f0a2e87fbd7b193b73e5005166fba5
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-24 20:59:48 +0100

    first-step

---
+++ Game.py
@@ -0,0 +1,17 @@
+
+'''
+
+Game:
+
+     - welcome message to user.
+     - show games.
+     - user can play : game number
+     - option to exit.
+     - ask play again.
+
+     Games:
+     - sentence no duplicate.
+     -names ---> list[count]
+     - x, y: 1 ---> 100
+
+'''
