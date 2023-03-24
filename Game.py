3764793114121a934b6190f5f7881c6ecb828df5
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-24 21:45:01 +0100

    second-step

--- Game.py
+++ Game.py
@@ -15,3 +15,23 @@ Game:
      - x, y: 1 ---> 100
 
 '''
+
+
+def no_duplicate(sentence):
+    words = sentence.split(' ')
+    new_words = []
+    for w in words:
+        if w in new_words:
+            continue
+        else:
+            new_words.append(w)
+            
+    new_sentence = ' '.join(new_words)
+    print(new_sentence)
+
+    
+    #print(' '.join(new_words))   #print('-'.join(new_words))
+    
+
+
+no_duplicate('my name is is is alex')
