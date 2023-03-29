c03866251a56063d78f81778e84d822368836a13
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-29 22:55:33 +0200

    fivth-step

--- Game.py
+++ Game.py
@@ -16,67 +16,76 @@ Game:
 
 '''
 
+class Game:
+    def __init__(self):
+        print(''' Welcome to our Game:
+    1- Sentence No Duplicate.
+    2- Names List Count.
+    3- Divided By.
+    4- to exit.
+''')
+        user_choice = int(input('Enter Game Number: '))
+        if user_choice == 1:
+            sentence = input('Enter Sentence: ')
+            self.no_duplicate(sentence)
 
-def no_duplicate(sentence):
-    words = sentence.split(' ')
-    new_words = []
-    for w in words:
-        if w in new_words:
-            continue
+        elif user_choice == 2:
+            names = eval(input('Enter Names : '))
+            self.names_count(names)
+
+        elif user_choice == 3:
+            first_number = int(input('Enter First Number: '))
+            second_number = int(input('Enter Second Number: '))
+            self.divided_by(first_number, second_number)
+
+        elif user_choice == 4:
+            print('Goodbey ...')
+            return
+            
         else:
-            new_words.append(w)
-            
-    new_sentence = ' '.join(new_words)
-    print(new_sentence)
+            print('please enter numbers between 1:3')
+        
+    def no_duplicate(self, sentence):
+        words = sentence.split(' ')
+        new_words = []
+        for w in words:
+            if w in new_words:
+                continue
+            else:
+                new_words.append(w)
+                
+        new_sentence = ' '.join(new_words)
+        print(new_sentence)
+
+
+    def names_count(self, names):
+        count = []
+        for n in names:
+            count.append(len(n))
+        print(count)
+    
+    def divided_by(self, x, y):
+        result = []
+        for n in range(1, 101):
+            if n%x == 0 and n%y == 0:
+                result.append(n)  
+        print(result)
+
+g1 = Game()
 
     
-    #print(' '.join(new_words))   #print('-'.join(new_words))
     
 
 
-no_duplicate('my name is is is alex')
 
 
 
 
+#no_duplicate('my name is is is alex')
 
-
-
-def names_count(names):
-    count = []
-    for n in names:
-        count.append(len(n))
-    print(count)
-
-names_count(['alex', 'anna', 'emma'])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def divided_by(x,y):
-    result = []
-    for n in range(1, 101):
-        if n%x == 0 and n%y == 0:
-            result.append(n)
-
-            
-    print(result)
-
-
-
-
-divided_by(5,6)
+#names_count(['alex', 'anna', 'emma'])
+        
+#divided_by(5,6)
 
 
 
